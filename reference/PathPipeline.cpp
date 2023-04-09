955ba6d0fb618b35a4717ff7d0b034028837ec87
grady <mgradysaunders@gmail.com>
2023-04-09 16:52:58 -0400

    remove reference

--- reference/PathPipeline.cpp
+++
@@ -1,9 +0,0 @@
-#include "dirsig5/internal/core/paths/PathPipeline.h"
-
-namespace d5::core {
-
-size_t PathPipeline::stateWavelengthCount = 0;
-
-size_t PathPipeline::stateWavelength = 0;
-
-} // namespace d5::core
